621d41e979d62d624240d65345b753627f0ddbf8
risha-thimmancherla <risha.thimmancherla@bizacuity.com>
2025-09-14 20:23:31 -0500

    Book collection 4

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,7 @@
 class Book < ApplicationRecord
+    has_many :users, through: :user_books
+    has_many :user_books
+
     validates :title, presence: true # title must be present
     validates :author, presence: true # author must be present
     validates :price, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true # price must be non-negative if present
